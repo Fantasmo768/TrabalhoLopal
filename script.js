436ba7b97c56389f88dc485b049017a8ea21d006
unknown <sopesquisa133@gmail.com>
2025-06-16 14:46:36 -0300

    Versão branch

--- script.js
+++ script.js
@@ -2,40 +2,54 @@ let promptSync = require('prompt-sync')
 let fs = require('fs')
 let path = require('path')
 
+//#region variaveis
 let nomes = []; //Vetor para armazenar valores dos nomes
 let endereços = []; //Vetor para armazenar valores dos endereços
 let distancias = []; //Vetor para armazenar valores das distâncias
 let valores = []; //Vetor para armazenar valores dos valores
 let tipos = [];
 let rodarCodigo = true;
-let quantidade = 0; 
+let quantidade = 0;
 let calculos = []; //Vetor para armazenar valores dos cálculos
 let calculo = 0;
+//#endregion
 
-
-while (rodarCodigo==true) {
-    let nome = promptSync(`Digite o nome do ${quantidade}° cliente (digite "parar" no nome para parar o programa)`); //
+while (rodarCodigo == true) {
+    
+    //#region Nome
+    let nome = promptSync(`Digite o nome do ${quantidade}° cliente (digite "parar" no nome para parar o programa)`);
     if (nome == "parar") {
-        rodarCodigo=false
+        rodarCodigo = false
     } else if (!isNaN(nome)) {
         nome = promptSync(`Por favor, não digite nenhum número`)
     }
-    let endereço = promptSync(`Digite o endereço do ${quantidade+1}° cliente`); 
+    //#endregion
+
+    //#region endereço
+    region
+    let endereço = promptSync(`Digite o endereço do ${quantidade + 1}° cliente`);
     if (!isNaN(endereço)) {
         endereço = promptSync(`Por favor, não digite nenhum número`)
     }
+    //#endregion
+
+    //#region distancia
     let distancia = parseInt(promptSync(`Digite a distância de entrega do ${quantidade}° cliente (em quilometros)`));
     if (isNaN(distancia)) {
         distancia = promptSync(`Por favor, não digite letras`)
     }
-    let valor = parseInt(promptSync(`Digite o valor de cada KM do ${quantidade+1}° cliente (em reais)`));
+    //#endregion
+
+    //#region valor
+    let valor = parseInt(promptSync(`Digite o valor de cada KM do ${quantidade + 1}° cliente (em reais)`));
     if (isNaN(valor)) {
         valor = promptSync(`Por favor, não digite letras`)
     }
-    let tipo = promptSync(`Digite o tipo de entrega do ${quantidade+1}° cliente (escolha entre normal e urgente)`);
+    //#endregion
+    let tipo = promptSync(`Digite o tipo de entrega do ${quantidade + 1}° cliente (escolha entre normal e urgente)`);
 
-    nomes [quantidade] = nome;
-    endereços [quantidade] = endereço;
+    nomes[quantidade] = nome;
+    endereços[quantidade] = endereço;
     distancias[quantidade] = distancia;
     valores[quantidade] = valor;
     tipos[quantidade] = tipo;
