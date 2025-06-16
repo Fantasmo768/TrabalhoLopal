2f4dfa8c86ca31093a36bff5e170fd9e11b0ad06
unknown <sopesquisa133@gmail.com>
2025-06-16 13:54:46 -0300

    Versão 1.1

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ let distancias = []
 let valores = []
 let tipo = ""
 let quantidade = 0;
-
+let calculos = []
+let calculo = 0;
 while (true) {
-    let nome = promptSync()
-    let endereço = 
-    let distancia = 
-    let valor
-    tipo = 
+    let nome = promptSync(`Digite o nome do ${quantidade}° cliente`)
+    let endereço = promptSync(`Digite o endereço do ${quantidade+1}° cliente`)
+    let distancia = parseInt(promptSync(`Digite a distância de entrega do ${quantidade}° cliente`))
+    let valor = parseInt(promptSync(`Digite o valor de cada KM do ${quantidade+1}° cliente`))
+    tipo = promptSync(`Digite o tipo de entrega do ${quantidade+1}° cliente (escolha entre normal e urgente)`)
+
+    if (tipo == "normal") {
+        calculo = distancia * valor
+    }
 }
